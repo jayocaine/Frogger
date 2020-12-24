e208136c6f9c1bfd24154f2a4aca77823ba93092
unknown <dan@hololabs.org>
2020-12-23 21:13:19 -0800

    Seperated walk driving component from move to transform, and added smooth camera

---
+++ FroggerRepo/Assets/DriveWalkingAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveWalkingAnimation : MonoBehaviour
+{
+    Vector3 lastPosition;
+    public string walkingAnimatorParameter = "isWalking";
+    public Animator animator;
+
+    bool isMoving = false;
+    private void Awake() {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool wasMoving = isMoving;
+
+        isMoving = Vector3.Distance(transform.position, lastPosition) > 0.001f;
+        lastPosition = transform.position;
+
+        if (isMoving && !wasMoving) {
+            animator.SetBool(walkingAnimatorParameter, true);
+        } else if (!isMoving && wasMoving) {
+            animator.SetBool(walkingAnimatorParameter, false);
+        }
+
+    }
+}
--- FroggerRepo/Assets/Scripts/MoveToTransform.cs
+++ FroggerRepo/Assets/Scripts/MoveToTransform.cs
@@ -7,25 +7,9 @@ public class MoveToTransform : MonoBehaviour
 {
     public Transform follow;
     public float speed;
-    public string walkingAnimatorParameter = "isWalking";
 
-    public Animator animator;
-
-    bool isMoving = false;
-    // Update is called once per frame
     void Update()
     {
-        bool wasMoving = isMoving;
-
-        isMoving = Vector3.Distance(transform.position, follow.position) > 0.001f;
-        
-        if ( isMoving && !wasMoving) {
-            animator.SetBool(walkingAnimatorParameter, true);
-        } else if ( !isMoving && wasMoving) {
-            animator.SetBool(walkingAnimatorParameter, false);
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, follow.position, Time.deltaTime * speed);
-        
+        transform.position = Vector3.MoveTowards(transform.position, follow.position, Time.deltaTime * speed);        
     }
 }
