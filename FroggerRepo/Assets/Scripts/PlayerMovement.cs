c2fc40e3c2648290cffc222bb9fcec0552fa14b8
jayocaine <66975498+jayocaine@users.noreply.github.com>
2020-12-23 16:15:22 -0800

    set up the shit and stuff

---
+++ FroggerRepo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public UnityEvent fall;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+    void DetectInput()
+    {
+        Vector3 oldPosition = transform.localPosition; //determine previous position after moving
+               
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right);
+        }
+        if(oldPosition != transform.localPosition)
+        {           
+            //we have changed position
+           if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, Mathf.Infinity))
+           {
+                transform.parent = hit.transform;
+           }
+            else
+            {
+                //died
+                fall.Invoke();
+                print("dead ");
+            }
+        }
+    }
+}
